55baa8f0470ef0d6f37f46c6c2d1080d0b126572
mounvip1525 <mounvip1525@gmail.com>
2021-05-28 13:17:17 +0530

    starter code

--- LightsOut/src/Board.js
+++ LightsOut/src/Board.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react'
 import './Board.css';
 
 export default class Board extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    createBoard(){
+        let board = [];
+        return board
+    }
+    flipCellsAround(coord) {
+        let { nCols,nRows } = this.props;
+        let board = this.state.board;
+        let [ y,x ] = coord.split("-").map(Number); 
+
+        function flipCell(y,x) {
+            if(x >= 0 && x < nCols && y >= 0 && y < nRows){
+                board[y][x] = !board[y][x];
+            }
+        }
+        this.setState({board,hasWon});
+    }
     render() {
         return (
             <div>

f97558fcc69b7b4ce9d5192f0aaf07efe0e2fb49
mounvip1525 <mounvip1525@gmail.com>
2021-05-28 14:08:30 +0530

    board display and cell lit

--- LightsOut/src/Board.js
+++ LightsOut/src/Board.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import './Board.css';
+import Cell from './Cell';
 
 export default class Board extends Component {
+    static defaultProps = {
+        nRows:5,
+        nCols:5,
+        chanceLightStartsOn:0.25
+    }
     constructor(props){
         super(props)
         this.state = {
-
+            hasWon:false,
+            board:this.createBoard()
         }
     }
     createBoard(){
         let board = [];
+        for(let y=0; y < this.props.nRows; y++){
+            let row = [];
+            for(let x=0; x < this.props.nCols ; x++){
+                row.push(Math.random() < this.props.chanceLightStartsOn)
+            }
+            board.push(row);
+        }
         return board
     }
     flipCellsAround(coord) {
@@ -22,12 +36,25 @@ export default class Board extends Component {
                 board[y][x] = !board[y][x];
             }
         }
-        this.setState({board,hasWon});
+        // this.setState({board,hasWon});
     }
     render() {
+        let tableBody = [];
+        for(let y=0; y < this.props.nRows; y++){
+            let row = [];
+            for(let x=0; x < this.props.nCols ; x++){
+                let coord = `${y}-${x}`;
+                row.push(<Cell key={coord} isLit={this.state.board[y][x]} />);
+            }
+            tableBody.push(<tr key={y}>{row}</tr>);
+        }
         return (
             <div>
-                
+                <table className="Board">
+                    <tbody>
+                        {tableBody}
+                    </tbody>
+                </table>
             </div>
         )
     }

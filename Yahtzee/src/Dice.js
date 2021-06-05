73ccf08941c1149a88477b6be10b808a8ee6aca5
mounvip1525 <mounvip1525@gmail.com>
2021-06-05 22:44:21 +0530

    animations dice

--- Yahtzee/src/Dice.js
+++ Yahtzee/src/Dice.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react'
-import './Dice.css'
-import Die from './Die';
+import React, { Component } from "react";
+import Die from "./Die";
+import "./Dice.css";
 
-export default class Dice extends Component {
-    render() {
-        return (
-            <div className='Dice'>
-                {this.props.dice.map((id,idx) =>
-                    <Die 
-                        handleClick={this.props.handleClick}
-                        val={id}
-                        locked={this.props.locked[idx]}
-                        idx={idx}
-                        key={idx} 
-                        disabled={this.props.disabled} />
-                )}
-            </div>
-        )
-    }
+class Dice extends Component {
+  render() {
+    return (
+      <div className='Dice'>
+        {this.props.dice.map((d, idx) => (
+          <Die
+            handleClick={this.props.handleClick}
+            val={d}
+            locked={this.props.locked[idx]}
+            idx={idx}
+            key={idx}
+            disabled={this.props.disabled}
+            rolling={this.props.rolling && !this.props.locked[idx]}
+          />
+        ))}
+      </div>
+    );
+  }
 }
+
+export default Dice;

c4653a3a88c48c73192ab2cbf29ad95840a893db
mounvip1525 <mounvip1525@gmail.com>
2021-06-05 22:55:37 +0530

    final commit

--- Yahtzee/src/Die.js
+++ Yahtzee/src/Die.js
@@ -17,7 +17,7 @@ class Die extends Component {
 
   render() {
     const { numberWords, locked, val, disabled, rolling } = this.props;
-    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-4x `;
     if (locked) classes += "Die-locked";
     if (rolling) classes += "Die-rolling";
     return (

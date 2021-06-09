605fbf2f35ba019ff9310eddb8b7e0d6c71432cd
mounvip1525 <mounvip1525@gmail.com>
2021-06-09 23:41:55 +0530

    api fetch

--- DadJokes/src/JokeList.js
+++ DadJokes/src/JokeList.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class JokeList extends Component {
+    static defaultProps = {
+        numOfJokesToGet: 10
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+            jokes : []
+        }
+    }
+    async componentDidMount(){
+        let jokes = [];
+        while(jokes.length < this.props.numOfJokesToGet) {
+            let res = await axios.get("https://icanhazdadjoke.com/",{
+                headers:{
+                    Accept:"application/json"
+                }
+            });
+            jokes.push(res.data.joke)
+        }
+        console.log(jokes);
+        this.setState({ jokes : jokes })
+    }
     render() {
         return (
-            <div>
-                
+            <div className="JokeList">
+                <h1>Jokes App</h1>
+                <div className="JokeList-jokes">
+                    {this.state.jokes.map(joke=>(
+                        <div>{joke}</div>
+                    ))}
+                </div>
             </div>
         )
     }

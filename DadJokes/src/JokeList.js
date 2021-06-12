eba3a425a638da0d659b5def4b03a7780e28ef89
mounvip1525 <mounvip1525@gmail.com>
2021-06-12 22:37:25 +0530

    local storage added

--- DadJokes/src/JokeList.js
+++ DadJokes/src/JokeList.js
@@ -11,10 +11,13 @@ export default class JokeList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            jokes : []
+            jokes : JSON.parse(window.localStorage.getItem("jokes") || "[]" )
         }
     }
-    async componentDidMount(){
+    componentDidMount(){
+        if(this.state.jokes.length === 0) this.getJokes();
+    }
+    async getJokes(){
         let jokes = [];
         while(jokes.length < this.props.numOfJokesToGet) {
             let res = await axios.get("https://icanhazdadjoke.com/",{
@@ -26,6 +29,7 @@ export default class JokeList extends Component {
         }
         console.log(jokes);
         this.setState({ jokes : jokes })
+        window.localStorage.setItem("jokes",JSON.stringify(jokes))
     }
     handleVote(id,delta) {
         this.setState(

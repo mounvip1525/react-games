3212d9b4c57271163d2d730760d41e479c0918ad
mounvip1525 <mounvip1525@gmail.com>
2021-06-13 22:35:07 +0530

    spinner added

--- DadJokes/src/JokeList.js
+++ DadJokes/src/JokeList.js
@@ -11,7 +11,8 @@ export default class JokeList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            jokes : JSON.parse(window.localStorage.getItem("jokes") || "[]" )
+            jokes : JSON.parse(window.localStorage.getItem("jokes") || "[]" ),
+            loading:false
         }
         this.handleClick = this.handleClick.bind(this);
     }
@@ -31,7 +32,8 @@ export default class JokeList extends Component {
         // console.log(jokes);
         // this.setState({ jokes : jokes })
         this.setState(st => ({
-            jokes: [...st.jokes,...jokes]
+            jokes: [...st.jokes,...jokes],
+            loading:false
         }),()=> window.localStorage.setItem("jokes",JSON.stringify(this.state.jokes)))
     }
     handleVote(id,delta) {
@@ -43,9 +45,18 @@ export default class JokeList extends Component {
         ,()=> window.localStorage.setItem("jokes",JSON.stringify(this.state.jokes)))
     }
     handleClick(){
-        this.getJokes();
+        this.setState({loading:true},this.getJokes)
+        // this.getJokes();
     }
     render() {
+        if(this.state.loading){
+            return(
+                <div className="JokeList-spinner">
+                    <i className="far fa-8x fa-laugh fa-spin" />
+                    <h1 className="JokeList-title">Loading...</h1>
+                </div>
+            )
+        }
         return (
             <div className="JokeList">
                 <div className="JokeList-sidebar">
